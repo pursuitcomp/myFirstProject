4871ebeeab2d67540baaf94d5d00532b807cabf7
pursuitcomp <pursuitcomp@live.com>
2018-04-17 13:30:20 -0500

    first project

---
+++ src/myFirstProject/MyName.java
@@ -0,0 +1,14 @@
+package myFirstProject;
+
+public class MyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Name: Simon Mwangi");
+System.out.println("AGE:28");
+System.out.println("1533 GALLOP LN"+'\n'+"St Louis"+ '\n'+ "MO 63152");
+
+
+	}
+
+}
